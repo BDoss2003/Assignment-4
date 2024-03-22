507a73f8dbda22b1ce91144aa7a2d9f57219e1c2
Brian Doss <bjdoss1@buffs.wtamu.edu>
2024-03-21 22:26:32 -0500

    Finished

--- Barky2024/test_commands.py
+++ Barky2024/test_commands.py
@@ -1,4 +1,4 @@
-# how would I test Barky?
+# How would I test Barky?
 # First, I wouldn't test barky, I would test the reusable modules barky relies on:
 # commands.py and database.py
 
@@ -9,4 +9,6 @@ def test_quit_command():
     pass
 
 # okay, should I test the other commands?
-# not really, they are tighly coupled with sqlite3 and its use in the database.py module
+# Not really, they are tighly coupled with sqlite3 and its use in the database.py module
+
+# No testing needed
